0299170553bc02947a26cf8c2fd4ff2e8f933a43
Canberk Sinangil <canberksinangil@gmail.com>
2020-08-17 14:51:31 +0000

    NotificationService and NotificationContent extensions are  written in Swift

---
+++ InsiderDemoWithSwiftTargets/InsiderNotificationContent/NotificationViewController.swift
@@ -0,0 +1,64 @@
+//
+//  NotificationViewController.swift
+//  InsiderNotificationContent
+//
+//  Created by Insider on 17.08.2020.
+//  Copyright © 2020 Insider. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+// FIXME: Please change with your app group.
+let APP_GROUP = "group.com.useinsider.InsiderDemo"
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension, iCarouselDelegate, iCarouselDataSource {
+    
+    @IBOutlet weak var carousel: iCarousel!
+    deinit {
+        carousel.delegate = nil
+        carousel.dataSource = nil
+    }
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return InsiderPushNotification.getNumberOfSlide()
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        return InsiderPushNotification.getSlide(index, reusing: view, superView: self.view)
+    }
+    
+    func carouselItemWidth(_ carousel: iCarousel) -> CGFloat {
+        return InsiderPushNotification.getItemWidth()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+        
+        InsiderPushNotification.interactivePushLoad(APP_GROUP, superView:self.view)
+        carousel.type = .rotary
+        carousel.reloadData()
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        InsiderPushNotification.interactivePushDidReceive()
+    }
+    
+    
+    func didReceive(
+        _ response: UNNotificationResponse,
+        completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
+        if (response.actionIdentifier == "insider_int_push_next") {
+            carousel.scrollToItem(at: InsiderPushNotification.didReceiveResponse(carousel.currentItemIndex), animated: true)
+            completion(.doNotDismiss)
+        } else {
+            InsiderPushNotification.logPlaceholderClick(response)
+            completion(.dismissAndForwardAction)
+        }
+    }
+    
+}
+
---
+++ InsiderDemoWithSwiftTargets/InsiderNotificationService/NotificationService.swift
@@ -0,0 +1,56 @@
+//
+//  NotificationService.swift
+//  InsiderNotificationService
+//
+//  Created by Insider on 17.08.2020.
+//  Copyright © 2020 Insider. All rights reserved.
+//
+
+import UserNotifications
+
+// FIXME: Please change with your app group.
+let APP_GROUP = "group.com.useinsider.InsiderDemo"
+
+class NotificationService: UNNotificationServiceExtension {
+    
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+    
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            
+            let nextButtonText = ">>"
+            let goToAppText = "Launch App"
+            
+            InsiderPushNotification.showInsiderRichPush(
+                request,
+                appGroup: APP_GROUP as String,
+                nextButtonText: nextButtonText,
+                goToAppText: goToAppText,
+                success: { attachment in
+                    if let attachment = attachment {
+                        bestAttemptContent.attachments = bestAttemptContent.attachments + [attachment as UNNotificationAttachment]
+                        print(bestAttemptContent.attachments)
+                    }
+                    print(bestAttemptContent.attachments)
+                    contentHandler(bestAttemptContent)
+                    print(bestAttemptContent.attachments)
+            })
+            print(bestAttemptContent.attachments)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+}
+
