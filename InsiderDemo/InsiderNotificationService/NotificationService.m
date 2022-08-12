0c872182834bd3e997cbf61898a335230d6a5126
sonerdm <soner@useinsider.com>
2022-08-12 08:19:51 +0300

    insider swift demo update

--- InsiderDemo/InsiderNotificationService/NotificationService.m
+++ InsiderDemo/InsiderNotificationService/NotificationService.m
@@ -1,11 +1,3 @@
-//
-//  NotificationService.m
-//  InsiderNotificationService
-//
-//  Created by Insider on 30.03.2020.
-//  Copyright © 2021 Insider. All rights reserved.
-//
-
 #import "NotificationService.h"
 #import <InsiderMobileAdvancedNotification/InsiderPushNotification.h>
 
@@ -14,46 +6,32 @@
 @property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
 @property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
 
-@property (nonatomic, strong) UNNotificationRequest *receivedRequest;
-@property (nonatomic, strong) NSString *source;
-
 @end
 
-// DO NOT FORGET to change this to your app group
+// FIXME: Please change with your app group.
 static NSString *APP_GROUP = @"group.com.useinsider.InsiderDemo";
 
 @implementation NotificationService
-- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
-    self.receivedRequest = request;
+
+-(void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
     self.contentHandler = contentHandler;
     self.bestAttemptContent = [request.content mutableCopy];
-    NSDictionary *notificationData = request.content.userInfo;
-    self.source = [notificationData objectForKey:@"source"];
     
-    // YOU CAN CUSTOMIZE THESE
+    // MARK: You can customize these.
     NSString *nextButtonText = @">>";
     NSString *goToAppText = @"Launch App";
     
-    if ([self.source  isEqual: @"Insider"]) {
-      [InsiderPushNotification showInsiderRichPush:request appGroup:APP_GROUP nextButtonText:nextButtonText goToAppText:goToAppText  success:^(UNNotificationAttachment *attachment) {
-          self->_bestAttemptContent.attachments = [self->_bestAttemptContent.attachments arrayByAddingObject:attachment];
-          self.contentHandler(self.bestAttemptContent);
-      }];
-      
-      return;
-    }
-  
-    //other push provider code
-    
+    [InsiderPushNotification showInsiderRichPush:request appGroup:APP_GROUP nextButtonText:nextButtonText goToAppText:goToAppText success:^(UNNotificationAttachment *attachment) {
+        if (attachment) {
+            self.bestAttemptContent.attachments = [NSArray arrayWithObject:attachment];
+        }
+        
+        self.contentHandler(self.bestAttemptContent);
+    }];
 }
-- (void)serviceExtensionTimeWillExpire {
-  if ([self.source  isEqual: @"Insider"]) {
+
+-(void)serviceExtensionTimeWillExpire {
     self.contentHandler(self.bestAttemptContent);
-    
-    return;
-  }
-  
-  //Other push provider code
- 
 }
+
 @end
